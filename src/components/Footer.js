f9cf9db15e1fa842f2fd9a079a9e7a1dceb81768
mvillagordo <m.villagordovera@gmail.com>
2020-09-28 22:58:13 +0200

    Se añade link descarga CV en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FaCloudDownloadAlt } from 'react-icons/fa';
 import styled from 'styled-components';
+
+const dowmload = 'https://www.dropbox.com/s/1z9a7hrdxkjp174/CV-MANUEL.pdf?dl=0';
 
 const ContainerFooter = styled.div`
     bottom: 0;
@@ -14,6 +17,7 @@ const Footer = () => {
         <ContainerFooter className="footer py-2 text-center font-global bg-fit-white-300 text-fit-dark-500 shadow">
             <footer>
                 <p>Deploy with <a href="https://vercel.com" title="Vercel">Vercel</a></p>
+                <small> <a href={dowmload} download="CV-Manuel.pdf">Descarga mi CV</a></small>
             </footer>
         </ContainerFooter>
     )
